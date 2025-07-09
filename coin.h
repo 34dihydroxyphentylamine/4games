95bcbb6737bb0238205d4dd685bc2891567b8e5e
34dihydroxyphentylamine <102799272+34dihydroxyphentylamine@users.noreply.github.com>
2025-07-09 17:12:18 +0200

    Create coin.h

---
+++ coin.h
@@ -0,0 +1,18 @@
+#ifndef COIN_H
+#define COIN_H
+
+#include <SDL.h>
+#include <vector>   
+#include <string>   
+
+bool initCoinSystem(SDL_Renderer* renderer);
+
+void draw_Coin(int x, int y, float scale, SDL_Renderer* renderer, Uint32 currentTime);
+
+int getCoinRenderedWidth(float scale);
+
+int getCoinRenderedHeight(float scale);
+
+void closeCoinSystem();
+
+#endif
